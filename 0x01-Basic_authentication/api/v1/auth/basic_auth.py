d5d38881bba73567791fba5a0b7b264c58cef63e
Godwin Okpe <okpegodwin18@yahoo.com>
2024-02-14 14:02:53 +0000

    Added more functionality to the authentication system: Basic auth decoding in base64

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Basic Auth module
+"""
+import base64
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic auth class that inherits from Auth
+    """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns Base64 part of the Authorization header for
+        a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Method that returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_value = base64.b64decode(base64_authorization_header) \
+                            .decode('utf-8')
+            return decoded_value
+        except base64.binascii.Error:
+            return None
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unathorized() -> str:
+    """
+    Raises an unauthorized 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """
+    Raises a Forbidden 403 error
+    """
+    abort(403)
