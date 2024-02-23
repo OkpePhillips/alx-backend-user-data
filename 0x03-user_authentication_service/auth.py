15273f5ece8b72dbb37cfff2ecedb8dd869f3619
Godwin Okpe <okpegodwin18@yahoo.com>
2024-02-23 09:45:12 +0000

    Fixed raising valueError exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -112,10 +112,10 @@ class Auth:
         """
         Update user's password using reset token.
         """
-        user = self._db.find_user_by(reset_token=reset_token)
-        if user:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(new_password)
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
-        else:
+        except NoResultFound:
             raise ValueError("Reset token does not exist.")
